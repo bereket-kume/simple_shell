d8938306a1d68c315a8fd16996b57e46980af4f2
bereket-kume <bereketkume@gmail.com>
2023-11-05 03:55:26 -0800

    done

--- main.c
+++ main.c
@@ -1,13 +1,17 @@
 #include "main.h"
- int main()
-{
+
+ int main(){
 	char *lineptr = NULL;
 	size_t n = 0;
-	while(getline(&lineptr, &n, stdin) != EOF)
+	int read;
+	while((read = getline(&lineptr, &n, stdin)) != -1)
 	{
-		int length = strlen(lineptr);
-		lineptr[length-1] = '\0';
+	if (read > 0 && lineptr[read - 1] == '\n')
+	{
+		lineptr[read-1] = '\0';
+	}
 		printf("$ ");
 	}
+	free(lineptr);
 	return (0);
 }
