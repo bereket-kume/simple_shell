0a8b59af04d2da5142550353cc1000c3ab9c4079
bereket-kume <bereketkume@gmail.com>
2023-11-05 03:07:34 -0800

    done

---
+++ main.c
@@ -0,0 +1,6 @@
+#include "main.h"
+ int main()
+{
+	display();
+	return (0);
+}
--- main.h
+++ main.h
@@ -7,6 +7,6 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-
-
+#include <string.h>
+void display();
 #endif
