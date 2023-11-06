44924d532e6c946e7c402859c80b8f794add2839
bereket-kume <bereketkume@gmail.com>
2023-11-06 08:48:13 -0800

    done

--- getline_fun.c
+++ getline_fun.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#define DELIM " \t\r\n\a"
  void getline_fun(){
 	char *lineptr = NULL;
 	size_t n = 0;
@@ -7,9 +7,9 @@
 	while( getline(&lineptr, &n, stdin) != EOF)
 	{ 
 		int length = strlen(lineptr);
+		char *token;
 		lineptr[length - 1] = '\0';
-		printf("$ ");
+		token = strtok(lineptr, DELIM);
+		printf("$$%s ", token);
 	}
-	exit(EXIT_SUCCESS);
-
 }
