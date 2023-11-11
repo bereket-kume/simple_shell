23def571b655d91c328d348b0d5c324cabdbbdba
Abdiwak Amsalu <140180098+abdiwak34@users.noreply.github.com>
2023-11-11 00:04:43 -0800

    Update custom_strtok.c

--- custom_strtok.c
+++ custom_strtok.c
@@ -1,3 +1,12 @@
+#inlude "main.h"
+
+/**
+ * my_strtok - tokenize the entered string
+ * @str: the string entered
+ * @delimeters: the character based on which the string divided
+ * Return: tokenstart if the string isn't null else NULL
+/
+
 char *my_strtok(char *str, const char *delimiters) {
     static char *lastToken = NULL;
     char *tokenStart;
