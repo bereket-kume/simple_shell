1989a711ad577f7be87fb51c53adb4d258d1d187
Abdiwak Amsalu <140180098+abdiwak34@users.noreply.github.com>
2023-11-15 14:48:53 -0800

    Update ex_command.c

--- ex_command.c
+++ ex_command.c
@@ -9,45 +9,11 @@ void ex_command(char **args)
 	char *command = NULL;
 	char *command_kan = NULL;
 	int status;
-	pid_t pid = fork();
 
 	if (args && args[0])
 	{
-		command = args[0];
-		if (strchr(command, '/') != NULL)
-		{
-			command_kan = strdup(command);
-		}
-		else
-		{
-			char *path = getenv("PATH");
-			char *copy_path = strdup(path);
-			char *dir = strtok(copy_path, ":");
+		pid_t pid = fork();
 
-			while (dir != NULL)
-			{
-				command_kan = malloc(strlen(dir) + strlen(command) + 2);
-				sprintf(command_kan, "%s/%s", dir, command);
-				if (access(command_kan, X_OK) == 0)
-				{
-					break;
-				}
-				free(command_kan);
-				dir = strtok(NULL, ":");
-			}
-			if (strcmp("exit", command) == 0)
-			{
-				exit(EXIT_FAILURE);
-			}
-			if (dir == NULL)
-			{
-
-				fprintf(stderr, "Command not found in PATH: %s\n", command);
-				free(copy_path);
-				exit(EXIT_SUCCESS);
-			}
-			free(copy_path);
-		}
 		if (pid == -1)
 		{
 			perror("fork error");
@@ -66,7 +32,6 @@ void ex_command(char **args)
 				fprintf(stderr, "command not found in PATH: %s\n", command);
 				exit(EXIT_FAILURE);
 			}
-		}
 			if (execve(command_kan, args, NULL) == -1)
 			{
 				perror("execve error");
