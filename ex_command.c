a2e123a4504d992711fffb3c7af40f0416bffd75
bereket-kume <135522204+bereket-kume@users.noreply.github.com>
2023-11-15 14:59:24 -0800

    Update ex_command.c

--- ex_command.c
+++ ex_command.c
@@ -9,28 +9,10 @@ void ex_command(char **args)
 	char *command = NULL;
 	char *command_kan = NULL;
 	int status;
+	pid_t pid = fork();
 
 	if (args && args[0])
 	{
-		command = args[0];
-		if (strchr(command, '/') != NULL)
-		{
-			command_kan = strdup(command);
-		}
-		else
-		{
-			char *path = getenv("PATH");
-			char *copy_path = strdup(path);
-			char *dir = strtok(copy_path, ":");
-
-			while (dir != NULL)
-			{
-				command_kan = malloc(strlen(dir) + strlen(command) + 2);
-				sprintf(command_kan, "%s/%s", dir, command);
-				if (access(command_kan, X_OK) == 0)
-				{
-					break;
-				}
 				free(command_kan);
 				dir = strtok(NULL, ":");
 			}
@@ -40,14 +22,17 @@ void ex_command(char **args)
 			}
 			if (dir == NULL)
 			{
-
 				fprintf(stderr, "Command not found in PATH: %s\n", command);
 				free(copy_path);
-				exit(EXIT_SUCCESS);
+				exit(EXIT_FAILURE);
 			}
 			free(copy_path);
 		}
-		pid_t pid = fork();
+
+		if (strcmp(command, "exit") == 0)
+		{
+			exit(EXIT_FAILURE);
+		}
 
 		if (pid == -1)
 		{
@@ -56,30 +41,16 @@ void ex_command(char **args)
 		}
 		else if (pid == 0)
 		{
-			command = args[0];
-			command_kan = path_barbadi(command);
-			if (strcmp(command, "exit") == 0)
-			{
-				exit_handle();
-			}
-			if (command_kan == NULL)
-			{
-				fprintf(stderr, "command not found in PATH: %s\n", command);
-				exit(EXIT_FAILURE);
-			}
-		}
-		waitpid(pid, &status, 0);
 			if (execve(command_kan, args, NULL) == -1)
 			{
 				perror("execve error");
 				exit(EXIT_FAILURE);
 			}
+			else
+			{
+				waitpid(pid, &status, 0);
+			}
+			free(command_kan);
 		}
-		else
-		{
-			waitpid(pid, &status, 0);
-		}
-		free(command_kan);
 	}
-
 }
