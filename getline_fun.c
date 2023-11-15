039b877360c81bfd9b92cf3ff44718af51af35f6
Abdiwak Amsalu <140180098+abdiwak34@users.noreply.github.com>
2023-11-15 14:46:29 -0800

    Update getline_fun.c

--- getline_fun.c
+++ getline_fun.c
@@ -1,29 +1,22 @@
 #include "main.h"
 #define DELIM " \t\r\n\a"
+
 /**
  * getline_fun - function that parses input from the user
  * Return: nothing
  */
-void getline_fun(int argc, char *argv[])
+void getline_fun(void)
 {
     char *lineptr = NULL, *token, *args[64];
     size_t n = 0;
     int i;
-    if (argc > 1)
-    {
-	    for (i = 1; i < argc; i++)
-	    {
-		    args[i - 1] = argv[i];
-	    }
-	    args[argc-1] = NULL;
-	    ex_command(args);
-	    return;
-    }
+
     display_prompt();
 
     while (1)
     {
         ssize_t read_size = getline(&lineptr, &n, stdin);
+
         if (read_size == -1)
         {
             if (lineptr != NULL)
@@ -32,6 +25,7 @@ void getline_fun(int argc, char *argv[])
             }
             break; 
         }
+
         if (read_size > 0)
         {
             if (lineptr[read_size - 1] == '\n')
@@ -40,6 +34,7 @@ void getline_fun(int argc, char *argv[])
             }
             token = my_strtok(lineptr, DELIM);
             i = 0;
+
             while (token != NULL)
             {
                 args[i] = token;
@@ -49,8 +44,10 @@ void getline_fun(int argc, char *argv[])
             args[i] = NULL;
 	    ex_command(args);
         }
+
+        display_prompt();
     }
-    display_prompt();
+
     if (lineptr != NULL)
     {
         free(lineptr);
