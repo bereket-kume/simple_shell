dee1b92fadb74b123a58be8d67888687c3dde3eb
bereket-kume <135522204+bereket-kume@users.noreply.github.com>
2023-11-14 22:11:32 -0800

    Update getline_fun.c

--- getline_fun.c
+++ getline_fun.c
@@ -6,45 +6,47 @@
  */
 void getline_fun(void)
 {
-	char *lineptr = NULL, *token, *args[64];
-	size_t n = 0;
-	int i;
+    char *lineptr = NULL, *token, *args[64];
+    size_t n = 0;
+    int i;
 
-	display_prompt();
-	while (1)
-	{
-		ssize_t read = getline(&lineptr, &n, stdin);
+    display_prompt();
+    while (1)
+    {
+        ssize_t read_size = getline(&lineptr, &n, stdin);
 
-		if (read == -1)
-		{
-			if (lineptr == NULL)
-			{
-				free(lineptr);
-			}
-		}
-		if (read > 0)
-		{
-			if (lineptr[read - 1] == '\n')
-			{
-				lineptr[read - 1] = '\0';
-			}
-			token = my_strtok(lineptr, DELIM);
-			i = 0;
-			while (token != NULL)
-			{
-				args[i] = token;
-				i++;
-				token = my_strtok(NULL, DELIM);
-			}
-			args[i] = NULL;
-			ex_command(args);
-		}
+        if (read_size == -1)
+        {
+            if (feof(stdin))
+            {
+                printf("\n");
+                exit(EXIT_SUCCESS);
+            }
+            perror("getline error");
+            exit(EXIT_FAILURE);
+        }
+        if (read_size > 0)
+        {
+            if (lineptr[read_size - 1] == '\n')
+            {
+                lineptr[read_size - 1] = '\0';
+            }
+            token = my_strtok(lineptr, DELIM);
+            i = 0;
+            while (token != NULL)
+            {
+                args[i] = token;
+                i++;
+                token = my_strtok(NULL, DELIM);
+            }
+            args[i] = NULL;
+            ex_command(args);
+        }
 
-			display_prompt();
-	}
-	if (lineptr != NULL)
-	{
-		free(lineptr);
-	}
-	free(lineptr);
+        display_prompt();
+    }
+    if (lineptr != NULL)
+    {
+        free(lineptr);
+    }
 }
