eb3f6bca3197252e0904199b367365ac8f507444
bereket-kume <135522204+bereket-kume@users.noreply.github.com>
2023-11-14 22:41:41 -0800

    Update getline_fun.c

--- getline_fun.c
+++ getline_fun.c
@@ -1,29 +1,54 @@
 #include "main.h"
 #define DELIM " \t\r\n\a"
+
 /**
- *getline_fun - is function that parse that form the user
- *Retrun - nothing
+ * getline_fun - function that parses input from the user
+ * Return: nothing
  */
 void getline_fun(void)
 {
-	char *lineptr = NULL, *token, *args[64];
-	size_t n = 0;
-	int i;
-	display_prompt();
-	while (1)
-	{
-		ssize_t read = getline(&lineptr, &n, stdin);
-		if (read == -1)
-		{
-			if (lineptr == NULL)
-			{
-				free(lineptr);
-			}
-		}
-		if (read > 0)
-		{
-			if (lineptr[read - 1] == '\n')
-			{
-				lineptr[read - 1] = '\0';
-			}
-	
+    char *lineptr = NULL, *token, *args[64];
+    size_t n = 0;
+    int i;
+
+    display_prompt();
+
+    while (1)
+    {
+        ssize_t read_size = getline(&lineptr, &n, stdin);
+
+        if (read_size == -1)
+        {
+            if (lineptr != NULL)
+            {
+                free(lineptr);
+            }
+            break; 
+        }
+
+        if (read_size > 0)
+        {
+            if (lineptr[read_size - 1] == '\n')
+            {
+                lineptr[read_size - 1] = '\0';
+            }
+            token = my_strtok(lineptr, DELIM);
+            i = 0;
+
+            while (token != NULL)
+            {
+                args[i] = token;
+                i++;
+                token = my_strtok(NULL, DELIM);
+            }
+            args[i] = NULL;
+        }
+
+        display_prompt();
+    }
+
+    if (lineptr != NULL)
+    {
+        free(lineptr);
+    }
+}
