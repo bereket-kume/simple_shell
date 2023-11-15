bc780cf44ae5197f278dd8bcf23035985b3e10a6
bereket-kume <135522204+bereket-kume@users.noreply.github.com>
2023-11-14 22:10:27 -0800

    Update path_barbadi.c

--- path_barbadi.c
+++ path_barbadi.c
@@ -7,21 +7,19 @@
  */
 char *path_barbadi(const char *command)
 {
-	char *path = getenv("PATH");
-	char *token = strtok(path, ":");
+    char *path = getenv("PATH");
+    char *token = strtok(path, ":");
 
-	while (token != NULL)
-	{
-		char full_path[PATH_MAX];
+    while (token != NULL)
+    {
+        char full_path[PATH_MAX];
 
-		snprintf(full_path, sizeof(full_path), "%s/%s", token, command);
-		if (access(full_path, F_OK) == 0)
-		{
-			return (strdup(full_path));
-		}
-		token = strtok(NULL, ":");
-	}
-	return (NULL);
+        snprintf(full_path, sizeof(full_path), "%s/%s", token, command);
+        if (access(full_path, F_OK) == 0)
+        {
+            return strdup(full_path);
+        }
+        token = strtok(NULL, ":");
+    }
+    return NULL;
 }
-
-
