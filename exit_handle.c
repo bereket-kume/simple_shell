12be46465c3c3c2de89e2e66fddb72084dbb31da
bereket-kume <135522204+bereket-kume@users.noreply.github.com>
2023-11-15 16:37:49 -0800

    Update exit_handle.c

--- exit_handle.c
+++ exit_handle.c
@@ -1,9 +1,62 @@
 #include "main.h"
-/**
- *exit_handle - is function that used to exit the process
- *Return: nothing
- */
-void exit_handle(void)
+
+
+
+void handle_signal(int signal)
 {
-	exit(0);
+	(void) signal;
+	prints("\n$ ");
+	fflush(stdout);
+}
+
+
+int *_error(char *argv, int count, char *args)
+{
+	char *number;
+
+	number = _itoa(count, 10);
+
+	write(2, argv, _strlen(argv));
+	write(2, ": ", 2);
+	write(2, number, _strlen(number));
+	write(2, ": ", 2);
+	write(2, args, _strlen(args));
+	perror(" ");
+
+	return (0);
+}
+
+
+int _stat(char **cmd, char **menged)
+{
+	char *concat_str = NULL, *new_concat = NULL;
+	int kotari;
+	struct stat sb;
+
+	if (menged == NULL)
+	{
+		free(menged);
+		free(cmd);
+		return (0);
+	}
+	for (kotari = 0; menged[kotari] != NULL ; kotari++)
+	{
+		concat_str = str_concat(menged[kotari], "/");
+		new_concat = str_concat(concat_str, cmd[0]);
+
+		if (stat(new_concat, &sb) == 0 && (sb.st_mode & S_IXUSR))
+		{
+			cmd[0] = new_concat;
+			free(concat_str);
+			free(menged[0]);
+			free(menged);
+			return (1);
+		}
+
+		free(concat_str);
+		free(new_concat);
+	}
+	free(menged[0]);
+	free(menged);
+	return (0);
 }
