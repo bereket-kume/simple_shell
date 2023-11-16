8e238c9a95d0a77693371354714f9ad55346ebda
bereket-kume <135522204+bereket-kume@users.noreply.github.com>
2023-11-16 03:30:31 -0800

    Update shell.h

---
+++ shell.h
@@ -0,0 +1,15 @@
+int _putchar(char c);
+void prints(char *str);
+char *str_concat(char *s1, char *s2);
+char *_strcpy(char *dest, char *src);
+int _strlen(const char *s);
+int _strcmp(char *s1, char *s2);
+char *_strstr(char *str1, char *str2);
+char *_strdup(char *str);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_itoa(int num, int base);
+int _atoi(char *a);
+char **split_str(char *line, char **env);
+void prompt(void);
+char *read_input();
+void printenv(char **environ);
