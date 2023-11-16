10be7781f2c68c95bfb8b5598984a6ccb1667f63
Abdiwak Amsalu <140180098+abdiwak34@users.noreply.github.com>
2023-11-15 16:39:12 -0800

    Update putchar.c

--- putchar.c
+++ putchar.c
@@ -1,4 +1,4 @@
-#include <unistd.h>
+#include "main.h"
 
 /**
  * _putchar - writes the character c to stdout
@@ -10,4 +10,23 @@
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
+}
+
+
+
+/**
+ * prints - Prints a string
+ * @str: Pointer to the string to print
+ *
+ * Return: void
+ */
+void prints(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
 }
