2b047bc291da5f7012a632358ed566482aebbf92
bereket-kume <135522204+bereket-kume@users.noreply.github.com>
2023-11-15 16:36:43 -0800

    Update ex_command.c

--- ex_command.c
+++ ex_command.c
@@ -1,74 +1,38 @@
 #include "main.h"
-/**
- *ex_command - is function that execute commands
- *@args: is our input form command line
- *Return: Nothing
- */
-void ex_command(char **args)
+
+
+
+int execute_cmd(char **av, char **args, char **env, int ymh, int kotari)
 {
-	char *command = NULL;
-	char *command_kan = NULL;
+	pid_t pid;
 	int status;
-	if (args && args[0])
+
+	if (args == NULL)
+		return (-1);
+
+	pid = fork();
+	if (pid  < 0)
 	{
-		pid_t pid = fork();
-		if (pid == -1)
-		{
-			perror("fork error");
-			exit(EXIT_FAILURE);
-		}
-		else if (pid == 0)
-		{
-			command = args[0];
-			if (strchr(command, '/') != NULL)
-			{
-				command_kan = strdup(command);
-			}
-			else
-			{
-				char *path = getenv("PATH");
-				char *path_copy = strdup(path);
-				char *dir = strtok(path_copy, ":");
-
-				while(dir != NULL)
-				{
-					command_kan = malloc(strlen(dir) + strlen(command) + 2);
-					sprintf(command_kan, "%s/%s", dir, command);
-					if(access(command_kan, X_OK) == 0)
-					{
-						break;
-					}
-					free(command_kan);
-					dir = strtok(NULL, ":");
-				}
-				if (strcmp(command, "exit") == 0)
-				{
-					exit(EXIT_FAILURE);
-				}
-				if (args && args[0] && strcmp(args[0], "cd") == 0)
-				{
-					execute_cd(args[1]);
-					return;
-				}
-				if (dir == NULL)
-				{
-					fprintf(stderr, "command not found in PATH: %s\n", command);
-					free(path_copy);
-					exit(EXIT_SUCCESS);
-				}
-				free(path_copy);
-			}
-			if  (execve(command_kan, args, NULL) == -1)
-			{
-				perror("execve error");
-				exit(EXIT_FAILURE);
-			}
-		}
-		else
-		{
-			waitpid(pid, &status, 0);
-		}
-		free(command_kan);
+		perror("./hsh: ");
+		exit(1);
 	}
+	else if (pid == 0)
+	{
+		if (execve(args[0], args, env) == -1)
+		{
+			_error(av[0], kotari, args[0]);
+			free(args);
+			exit(1);
+		}
+		exit(0);
+	}
+	else
+	{
+		if (ymh == 1)
+			free(args[0]);
 
+		free(args);
+		waitpid(pid, &status, WUNTRACED);
+	}
+	return (1);
 }
