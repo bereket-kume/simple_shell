b9c5205a53102a1663fc139dbfc649185c0f7106
bereket-kume <135522204+bereket-kume@users.noreply.github.com>
2023-11-18 02:33:53 -0800

    Update main.h

--- main.h
+++ main.h
@@ -4,7 +4,14 @@
 #include "val.h"
 extern char **environ;
 #include "shell.h"
-#include "oneshell.h"
+void _puts(char *str);
+int execute_cmd(char **av, char **args, char **environ, int ymh, int kotari);
+char **search(char **environ);
+char **_which(char *fpath);
+void handle_signal(int signal);
+int _stat(char **cmd, char **menged);
+int *_error(char *argv, int count, char *args);
+void *_calloc(unsigned int nmemb, unsigned int size);
 
 
 #endif
