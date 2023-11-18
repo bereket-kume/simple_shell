eac89b8f0541890370b1e169c71f52b90468465f
bereket-kume <135522204+bereket-kume@users.noreply.github.com>
2023-11-18 05:12:42 -0800

    Update main.c

--- main.c
+++ main.c
@@ -6,31 +6,56 @@
  *@env: is third parameter
  *Return: nothing
  */
-int main(int ac __attribute__((unused)), char **av, char **env)
-{
-	char *sarara;
-	char **args, **path;
-	int lk = 0, key_value = 0;
-	(void) av;
-	signal(SIGINT, handle_signal);
 
-	for (;;)
-	{
-		prompt();
-		sarara = read_input();
-		args = split_str(sarara, env);
-		if ((_strcmp(args[0], "\n") != 0) && (_strcmp(args[0], "env") != 0))
-		{
-			lk += 1;
-			path = search(env);
-			key_value = _stat(args, path);
-			execute_cmd(av, args, env, key_value, lk);
-		}
-		else
-		{
-			free(args);
-		}
-		free(sarara);
-	}
-	return (0);
+int main(int ac __attribute__((unused)), char **av, char **env) {
+    char *sarara;
+    char **args, **path;
+    int lk = 0, key_value = 0;
+    signal(SIGINT, handle_signal);
+
+    for (;;) {
+        printf("$ ");
+        fflush(stdout);
+        size_t bufsize = 0;
+        getline(&sarara, &bufsize, stdin);
+
+        char *token;
+        token = strtok(sarara, " \t\r\n\a");
+        int i = 0;
+        args = malloc(64 * sizeof(char *));
+        while (token != NULL) {
+            args[i++] = token;
+            token = strtok(NULL, " \t\r\n\a");
+        }
+        args[i] = NULL;
+
+        if (args[0] != NULL) {
+            lk += 1;
+            path = search(env);
+            key_value = _stat(args, path);
+
+            if (strcmp(args[0], "cd") == 0) {
+                handle_cd(args); 
+            } else {
+                pid_t pid = fork();
+                if (pid < 0) {
+                    perror("Fork error");
+                    exit(EXIT_FAILURE);
+                } else if (pid == 0) {
+                    if (execvp(args[0], args) == -1) {
+                        perror("Execution error");
+                        exit(EXIT_FAILURE);
+                    }
+                    exit(EXIT_SUCCESS);
+                } else {
+                    wait(NULL);
+                }
+            }
+        }
+
+        free(args);
+        free(sarara);
+    }
+
+    return 0;
 }
