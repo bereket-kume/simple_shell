2105dd56bfa6b5ae04ec07b8df43c9e8dffbe7c4
bereket kume <bereketkume@gmail.com>
2023-11-18 06:52:57 -0800

    done

--- set_env.c
+++ set_env.c
@@ -1,101 +1,108 @@
 #include "main.h"
 
-// ... (previous code)
+int _myenv(info_t *info)
+{
+	print_list_str(info->env);
+	return (0);
+}
+char *_getenv(info_t *info, const char *name)
+{
+	list_t *node = info->env;
+	char *p;
 
-/**
- * set_env_var - sets or updates an environment variable
- * @variable: the name of the variable
- * @value: the value to set
- * @env: the environment variables array
- * Return: 0 on success, -1 on failure
- */
-int set_env_var(const char *variable, const char *value, char ***env) {
-    char *new_var;
-    int i;
-
-    // Check if the variable already exists
-    for (i = 0; (*env)[i] != NULL; i++) {
-        if (_strstr((*env)[i], variable) != NULL) {
-            // Variable found, update its value
-            free((*env)[i]);
-            new_var = str_concat(str_concat(variable, "="), value);
-            (*env)[i] = _strdup(new_var);
-            free(new_var);
-            return 0;  // Success
-        }
-    }
-
-    // Variable not found, add it to the environment
-    new_var = str_concat(str_concat(variable, "="), value);
-    *env = _realloc(*env, sizeof(char *) * (i + 2));
-    if (*env == NULL) {
-        perror("Error reallocating memory");
-        return -1;  // Failure
-    }
-    (*env)[i] = _strdup(new_var);
-    (*env)[i + 1] = NULL;
-    free(new_var);
-    return 0;  // Success
+	while (node)
+	{
+		p = starts_with(node->str, name);
+		if (p && *p)
+			return (p);
+		node = node->next;
+	}
+	return (NULL);
 }
 
-/**
- * shell_setenv - built-in setenv command
- * @args: command arguments
- * @env: environment variables array
- */
-void shell_setenv(char **args, char ***env) {
-    if (args[1] == NULL || args[2] == NULL) {
-        _error(args[0], 1, "Usage: setenv VARIABLE VALUE");
-        return;
-    }
-
-    if (set_env_var(args[1], args[2], env) == -1) {
-        fprintf(stderr, "Error setting environment variable\n");
-    }
+int _mysetenv(info_t *info)
+{
+	if (info->argc != 3)
+	{
+		_eputs("Incorrect number of arguements\n");
+		return (1);
+	}
+	if (_setenv(info, info->argv[1], info->argv[2]))
+		return (0);
+	return (1);
 }
 
-/**
- * unset_env_var - removes an environment variable
- * @variable: the name of the variable
- * @env: the environment variables array
- * Return: 0 on success, -1 on failure
- */
-int unset_env_var(const char *variable, char ***env) {
-    int i, j;
+int _myunsetenv(info_t *info)
+{
+	int i;
 
-    for (i = 0; (*env)[i] != NULL; i++) {
-        if (_strstr((*env)[i], variable) != NULL) {
-            free((*env)[i]);
-            // Remove the variable by shifting the array
-            for (j = i; (*env)[j + 1] != NULL; j++) {
-                (*env)[j] = (*env)[j + 1];
-            }
-            // Reallocate memory to remove the last element
-            *env = _realloc(*env, sizeof(char *) * i);
-            if (*env == NULL) {
-                perror("Error reallocating memory");
-                return -1;  // Failure
-            }
-            return 0;  // Success
-        }
-    }
+	if (info->argc == 1)
+	{
+		_eputs("Too few arguements.\n");
+		return (1);
+	}
+	for (i = 1; i <= info->argc; i++)
+		_unsetenv(info, info->argv[i]);
 
-    fprintf(stderr, "Error: %s not found in environment\n", variable);
-    return -1;  // Failure
+	return (0);
 }
 
-/**
- * shell_unsetenv - built-in unsetenv command
- * @args: command arguments
- * @env: environment variables array
- */
-void shell_unsetenv(char **args, char ***env) {
-    if (args[1] == NULL) {
-        _error(args[0], 1, "Usage: unsetenv VARIABLE");
-        return;
-    }
+int populate_env_list(info_t *info)
+{
+	list_t *node = NULL;
+	size_t i;
 
-    if (unset_env_var(args[1], env) == -1) {
-        fprintf(stderr, "Error unsetting environment variable\n");
-    }
+	for (i = 0; environ[i]; i++)
+		add_node_end(&node, environ[i], 0);
+	info->env = node;
+	return (0);
+}
+int interactive(info_t *info)
+{
+	return (isatty(STDIN_FILENO) && info->readfd <= 2);
+}
+
+int is_delim(char c, char *delim)
+{
+	while (*delim)
+		if (*delim++ == c)
+			return (1);
+	return (0);
+}
+
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
+
+int _atoi(char *s)
+{
+	int i, sign = 1, flag = 0, output;
+	unsigned int result = 0;
+
+	for (i = 0;  s[i] != '\0' && flag != 2; i++)
+	{
+		if (s[i] == '-')
+			sign *= -1;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			flag = 1;
+			result *= 10;
+			result += (s[i] - '0');
+		}
+		else if (flag == 1)
+			flag = 2;
+	}
+
+	if (sign == -1)
+		output = -result;
+	else
+		output = result;
+
+	return (output);
 }
